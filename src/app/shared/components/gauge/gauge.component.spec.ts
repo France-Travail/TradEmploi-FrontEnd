5968ee7ae87986c858541cf10885af308f46cdf0
Thomas Loiseau <thomas.loiseau@atos.net>
2020-03-10 15:10:04 +0100

    trying to do some Unit tests in gauge component

--- src/app/shared/components/gauge/gauge.component.spec.ts
+++ src/app/shared/components/gauge/gauge.component.spec.ts
@@ -2,24 +2,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GaugeComponent } from './gauge.component';
 
+import 'zone.js/dist/zone-testing';
+
 describe('GaugeComponent', () => {
   let component: GaugeComponent;
   let fixture: ComponentFixture<GaugeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GaugeComponent ]
-    })
-    .compileComponents();
+      declarations: [GaugeComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GaugeComponent);
     component = fixture.componentInstance;
+    component.duration = 5;
+    component.user = 'advisor';
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('text property should equal to Parlez maintenant', async () => {
+    expect(component.text).toEqual('Parlez maintenant');
+  });
+
+  it('should display Parlez maintenant in HTML', async () => {
+    expect(fixture.nativeElement.querySelector('p').textContent).toEqual('Parlez maintenant');
   });
 });
