09d235afbdac84ecaa519e3244eb39659447c380
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2020-04-17 11:05:28 +0200

    env deleted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyD4ZAtJqO803JkMYvxun7WmiuU22hn97dE",
-    authDomain: "pole-emploi-trad.firebaseapp.com",
-    databaseURL: "https://pole-emploi-trad.firebaseio.com",
-    projectId: "pole-emploi-trad",
-    storageBucket: "pole-emploi-trad.appspot.com",
-    messagingSenderId: "860693433183",
-    appId: "1:860693433183:web:6afdaa4433c69f098204ea"
+    apiKey: "xxxxxx",
+    authDomain: "xxxxxx",
+    databaseURL: "https://xxxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxxx",
+    messagingSenderId: "xxxxxx",
+    appId: "xxxxxx"
   },
-  apiKey: 'AIzaSyBraAcmG6FhlK4A9hCOfo96_s_OvYXEciQ'
+  apiKey: 'xxxxxx'
 };
 
 /*
