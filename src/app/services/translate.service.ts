74a962fee645459402d015b2fce7f2bb3fe0827d
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2020-04-27 17:18:51 +0200

    translate api

--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -12,51 +12,28 @@ export class TranslateService {
 
   public translate(text: string, speaker: string): Observable<string> {
     const target: string = speaker === 'advisor' ? this.guest.writtenLanguage.split('-')[0] : this.advisor.split('-')[0];
-    const provider: string = environment.api.provider;
+    const url = `https://translation.googleapis.com/language/translate/v2?key=${environment.gcp.apiKey}`;
     const data = {
-      query: `
-      {
-        translate(text:"${text}",target:"${target}", provider:${provider}) {
-          text
-        }
-      }`,
+      q: text,
+      target: target,
+      format: 'text',
     };
-    const url = `https://translation.googleapis.com/v3/innovation-214316:translateText?key=${environment.gcp.apiKey}`;
-    const dataTemp = {
-      contents: [text],
-      targetLanguageCode: target,
-    };
-
     return new Observable((observer) => {
-      axios
-        .post(url, dataTemp)
+      axios({
+        method: 'post',
+        headers: { 'content-type': 'application/json; charset=utf-8' },
+        data: data,
+        url: url,
+      })
         .then((response) => {
-          console.log('response :', response);
-          const res = response.data.data.translations[0].translatedText
+          const res = response.data.data.translations[0].translatedText;
           console.log('res :', res);
-          observer.next(response.data.data.translate[0].text);
+          observer.next(res);
           observer.complete();
         })
         .catch((error) => {
           observer.error('Traduction indisponible momentanément');
         });
     });
-    // return new Observable(observer => {
-    //   axios
-    //     .post(environment.api.graphqlUrl, data, {
-    //       auth: {
-    //         username: environment.api.login,
-    //         password: environment.api.password
-    //       },
-    //       timeout: 5000
-    //     })
-    //     .then(response => {
-    //       observer.next(response.data.data.translate[0].text);
-    //       observer.complete();
-    //     })
-    //     .catch(error => {
-    //       observer.error('Traduction indisponible momentanément');
-    //     });
-    // });
   }
 }
