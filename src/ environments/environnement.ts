80d3bdd0a342ac8b4fd1cc0fe0b3fcfa2c0bf12c
anis <anishezam@gmail.com>
2020-04-29 03:23:42 +0200

    update env

---
+++ src/ environments/environnement.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  name: 'local',
+  production: false,
+  firebaseConfig: {
+    apiKey: 'xxxxxx',
+    authDomain: 'xxxxxx',
+    databaseURL: 'https://xxxxxx',
+    projectId: 'xxxxxx',
+    storageBucket: 'xxxxxx',
+    messagingSenderId: 'xxxxxx',
+    appId: 'xxxxxx',
+  },
+  gcp: {
+    apiKey: 'xxxxxx',
+  },
+  sentry: {
+    url: 'xxxxxx',
+  },
+  firefunction: {
+    url: 'xxxxxx',
+    login: 'xxxxxx',
+    password: 'xxxxxx',
+  },
+};
