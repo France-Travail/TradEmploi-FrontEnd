1d81ef8f260af1ed60f45c8942833820e964ac4d
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2020-05-04 16:23:00 +0200

    error handeld

--- src/app/shared/components/record/record.component.ts
+++ src/app/shared/components/record/record.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 import { AudioRecordingService } from 'src/app/services/audio-recording.service';
 import { VOCABULARY } from 'src/app/data/vocabulary';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-record',
@@ -24,7 +25,7 @@ export class RecordComponent implements OnInit {
   public canSend: boolean = false;
   public inProgress: boolean = false;
 
-  constructor(private settingsService: SettingsService, private audioRecordingService: AudioRecordingService) {}
+  constructor(private settingsService: SettingsService, private audioRecordingService: AudioRecordingService, private toastService: ToastService) {}
 
   ngOnInit(): void {
     this.start();
@@ -34,17 +35,17 @@ export class RecordComponent implements OnInit {
     this.putTitle();
     this.record();
     this.recordBarLoad();
-  }
+  };
 
   record = () => {
     this.audioRecordingService.language = this.language;
     this.audioRecordingService.start();
-  }
+  };
 
   putTitle = () => {
     const language = this.user === 'advisor' ? this.settingsService.advisor.language : this.settingsService.guest.value.language;
     this.text = VOCABULARY.find((item) => item.isoCode === language).sentences.recordText;
-  }
+  };
 
   private recordBarLoad = () => {
     const value: number = 100 / (this.duration * 10);
@@ -69,18 +70,18 @@ export class RecordComponent implements OnInit {
         this.exitAudio();
       }
     }, 100);
-  }
+  };
 
   pauseOrResume = () => {
     this.isPaused = !this.isPaused;
-  }
+  };
 
   exitAudio = async () => {
     if (this.intervalId !== undefined) {
       this.stopRecord();
       this.exit.emit();
     }
-  }
+  };
 
   retry = async (): Promise<void> => {
     if (this.intervalId !== undefined) {
@@ -89,25 +90,47 @@ export class RecordComponent implements OnInit {
     this.width = 0;
     this.seconds = 0;
     this.start();
-  }
+  };
 
   sendSpeech = async (): Promise<void> => {
     this.inProgress = true;
     if (this.intervalId !== undefined) {
       this.stopRecord();
+      // this.audioRecordingService.speechToText.subscribe(
+      //   (res: ) => {
+      //     console.log('Entre');
+      //     this.inProgress = false;
+      //     this.send.emit(res);
+      //   },
+      //   (err) => {
+      //     console.log('Entre ERR');
+      //     this.inProgress = false;
+      //     //this.send.emit(err);
+      //     this.toastService.showToast('Une erreur a eu lieu. Merci de réessayer plus tard.', 'toast-error');
+      //   }
+      // );
       this.audioRecordingService.speechToText.subscribe(
-        (res) => {
+        (response) => {
+          console.log('Entre event', response);
           this.inProgress = false;
-          this.send.emit(res);
+          if (response.includes('error')) {
+            this.toastService.showToast('Traduction indisponible momentanément. Merci de réessayer plus tard.', 'toast-error');
+          } else {
+            this.send.emit(response);
+          }
         },
-        (err) => this.send.emit(err)
+        (err) => {
+          console.log('Entre ERR', err);
+          this.inProgress = false;
+          this.toastService.showToast('Traduction indisponible momentanément. Merci de réessayer plus tard.', 'toast-error');
+        }
       );
     }
-  }
+  };
 
   private stopRecord = () => {
     clearInterval(this.intervalId);
     this.intervalId = undefined;
     this.audioRecordingService.stop(this.seconds);
-  }
+  };
 }
