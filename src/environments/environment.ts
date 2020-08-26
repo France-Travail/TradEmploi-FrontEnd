b2a17b42c366de601e943fc25b45b9ab9cf3a9d6
daniel.sengmanivanh@pole-emploi.fr <innostarterkit@gmail.com>
2020-08-26 15:35:06 +0200

    deleted env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,23 +2,23 @@ export const environment = {
   name: 'prod',
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyD4ZAtJqO803JkMYvxun7WmiuU22hn97dE",
-    authDomain: "pole-emploi-trad.firebaseapp.com",
-    databaseURL: "https://pole-emploi-trad.firebaseio.com",
-    projectId: "pole-emploi-trad",
-    storageBucket: "pole-emploi-trad.appspot.com",
-    messagingSenderId: "860693433183",
-    appId: "1:860693433183:web:6afdaa4433c69f098204ea"
+    apiKey: "xxxxxx",
+    authDomain: "xxxxxx",
+    databaseURL: "xxxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxxx",
+    messagingSenderId: "xxxxxx",
+    appId: "xxxxxx"
   },
   gcp: {
-    apiKey: 'AIzaSyBraAcmG6FhlK4A9hCOfo96_s_OvYXEciQ'
+    apiKey: 'xxxxxx'
   },
   sentry:{
-    url:'https://953593ebcd8a40b4a2e743176c9488b8@sentry.io/5180549'
+    url:'xxxxxx'
   },
   firefunction:{
-    url:'https://us-central1-pole-emploi-trad.cloudfunctions.net/rates',
-    login: 'ludo', 
-    password: '_#Q5g_1i9Z6lRFC'
+    url:'xxxxxx',
+    login: 'xxxxxx', 
+    password: 'xxxxxx'
   }
 };
