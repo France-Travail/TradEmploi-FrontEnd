43351d0d6603953b765c765a0ef776e1de3c9812
anis <anishezam@gmail.com>
2020-08-26 15:04:25 +0200

    code review

--- src/app/services/crypt.service.ts
+++ src/app/services/crypt.service.ts
@@ -6,18 +6,6 @@ import { MessageWrapped } from '../models/translate/message-wrapped';
   providedIn: 'root',
 })
 export class CryptService {
-  constructor() {}
-
-  public encrypt(text: string, iv: string, key: string) {
-    return crypto.AES.encrypt(text, key, {
-      iv: crypto.enc.Hex.parse(iv),
-      mode: crypto.mode.CBC,
-      padding: crypto.pad.Pkcs7,
-    }).toString();
-  }
-  public decrypt(text: string, iv: string, key: string): string {
-    return crypto.AES.decrypt(text, key, { iv: crypto.enc.Hex.parse(iv), mode: crypto.mode.CBC, padding: crypto.pad.Pkcs7 }).toString(crypto.enc.Utf8);
-  }
 
   public encryptWrapped(messageWrapped: MessageWrapped, roomId: string): MessageWrapped {
     if (messageWrapped.message) {
@@ -26,11 +14,24 @@ export class CryptService {
     }
     return messageWrapped;
   }
+
   public decryptWrapped(messageWrapped: MessageWrapped, roomId: string): MessageWrapped {
     if (messageWrapped.message) {
       messageWrapped.message.text = this.decrypt(messageWrapped.message.text, messageWrapped.message.time.toString(), roomId);
       messageWrapped.message.translation = this.decrypt(messageWrapped.message.translation, messageWrapped.message.time.toString(), roomId);
     }
     return messageWrapped;
+  }
+
+  private encrypt(text: string, iv: string, key: string): string {
+    return crypto.AES.encrypt(text, key+iv, {
+      iv: crypto.enc.Hex.parse(iv),
+      mode: crypto.mode.CBC,
+      padding: crypto.pad.Pkcs7,
+    }).toString();
+  }
+
+  private decrypt(text: string, iv: string, key: string): string {
+    return crypto.AES.decrypt(text, key+iv, { iv: crypto.enc.Hex.parse(iv), mode: crypto.mode.CBC, padding: crypto.pad.Pkcs7 }).toString(crypto.enc.Utf8);
   }
 }
