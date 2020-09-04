ddc791548132b0c69852e586681acfdeb6df6b2d
daniel.sengmanivanh <daniel.sengmanivanh@gmail.com>
2020-09-04 16:02:05 +0200

    wording gdpr

--- src/app/models/gdpr.ts
+++ src/app/models/gdpr.ts
@@ -1,5 +1,12 @@
 export interface Gdpr {
-  privacityText: string;
+  title: string;
+  preDescription: string;
+  description: string;
+  privacyText: string;
   optionTitle: string;
   optionText: string;
+  disagreeBtn: string;
+  learnBtn: string;
+  agreeBtn: string;
+  confirmBtn: string;
 }
