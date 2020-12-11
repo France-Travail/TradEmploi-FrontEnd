c849096b164b4cb245e7fe967f67f1494bda4a37
innostarterkit <innostarterkit@gmail.com>
2020-12-11 15:58:53 +0100

    filter all and most done

--- src/app/pages/choice/choice-new.component.ts
+++ src/app/pages/choice/choice-new.component.ts
@@ -11,24 +11,20 @@ import { SettingsService } from 'src/app/services/settings.service';
 })
 export class ChoiceNewComponent {
 
-  public displayedColumns: string[];
-  public countries: Vocabulary[] = VOCABULARY;
+  public columns: string[] = ['country', 'flag', 'traduction', 'language'];
+  public countries: Vocabulary[] = [];
   public dataCountriesSource: MatTableDataSource<Vocabulary>;
+  public countriesSelected: string[] = ['en-GB', 'ar-XA', 'ps-AF', 'fa-IR', 'bn-BD', 'es-ES', 'de-DE', 'pt-PT', 'it-IT', 'zh-CN', 'ru-RU'];
 
   @ViewChild(MatSort, { static: true }) sorting: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private settingsService: SettingsService) {
-    this.displayedColumns = ['country', 'flag', 'traduction', 'language'];
-    this.countries = this.countries.filter((country) => country.isoCode !== 'ar-XA');
-    this.dataCountriesSource = new MatTableDataSource(this.countries);
-  }
+  constructor(private settingsService: SettingsService) {}
+
   ngOnInit() {
-    this.sorting.direction = 'asc';
-    this.sorting.active = 'countryNameFr';
-    this.dataCountriesSource.sort = this.sorting;
-    this.dataCountriesSource.paginator = this.paginator;
+    this.displayMost()
   }
+
   public applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataCountriesSource.filter = filterValue.trim().toLowerCase();
@@ -37,7 +33,6 @@ export class ChoiceNewComponent {
   public chooseLanguage(selectedCountry: string) {
     const voice = this.countries.filter((country) => country.isoCode === selectedCountry)[0];
     const audioCode = voice.audioCode ? voice.audioCode : voice.isoCode;
-    // this.voicesService.guest = { audio: audioCode, written: voice.isoCode , languageName: voice.languageNameFr};
     this.settingsService.user.next({ ...this.settingsService.user.value, language: { audio: audioCode, written: voice.isoCode, languageName: voice.languageNameFr }, connectionTime: Date.now() });
     if (localStorage.getItem('user') != null) {
       const user = JSON.parse(localStorage.getItem('user'));
@@ -55,4 +50,25 @@ export class ChoiceNewComponent {
     return isoCode.split('-')[1].toLowerCase();
   }
 
+  public displayMost(){
+    this.countries = this.countriesSelected.map((country) => VOCABULARY.find((i) => i.isoCode === country));
+    this.dataCountriesSource = new MatTableDataSource(this.countries);
+  }
+
+  public displayAll(){
+    this.countries = VOCABULARY.filter((country) => country.isoCode !== 'ar-XA')
+    this.dataCountriesSource = new MatTableDataSource(this.countries);
+    this.sorting.direction = 'asc';
+    this.sorting.active = 'countryNameFr';
+    this.dataCountriesSource.sort = this.sorting;
+    this.dataCountriesSource.paginator = this.paginator;
+  }
+
+  public displayList(){
+    
+  }
+
+  public displayGrid(){
+    
+  }
 }
