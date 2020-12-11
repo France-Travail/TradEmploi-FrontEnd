2dc78c4038ffedb966d6983087a2d6a43256e781
mariem <mariem.feki.8@gmail.com>
2020-12-11 14:10:34 +0100

    fix navbar big

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavbarService } from './services/navbar.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title: string = 'translation';
-  constructor(private router: Router) {
+  constructor(private router: Router, public navbarService: NavbarService) {
     addEventListener('storage', this.storageChange.bind(this));
   }
 
