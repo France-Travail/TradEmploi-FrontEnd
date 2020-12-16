1a503b010f86be8e540b78ef9a2bc10b84f712f1
innostarterkit <innostarterkit@gmail.com>
2020-12-16 18:39:35 +0100

    css on choice

--- src/app/pages/choice/choice-new.component.ts
+++ src/app/pages/choice/choice-new.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+import { NavbarService } from 'src/app/services/navbar.service';
 
 @Component({
   selector: 'app-choice-new',
   templateUrl: './choice-new.component.html',
   styleUrls: ['./choice-new.component.scss'],
 })
-export class ChoiceNewComponent {
+export class ChoiceNewComponent implements AfterContentInit{
 
   public search:String;
   public listSelected:Boolean = false;
   public displayAll:Boolean = false;
+
+  constructor(private navService: NavbarService){
+    this.navService.handleTabsChoice();
+  }
+
+  ngAfterContentInit(): void {
+    this.navService.show();
+  }
 
   public applyFilter(event: Event) {
     this.search = (event.target as HTMLInputElement).value;
