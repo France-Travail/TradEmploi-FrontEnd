a2d1597445a3d609a8312c5f0230bcc0e922aae3
anis <anishezam@gmail.com>
2021-03-01 12:41:00 +0100

    before cr

--- src/app/pages/translation/components/chat/chat.component.ts
+++ src/app/pages/translation/components/chat/chat.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Message } from 'src/app/models/translate/message';
 import { MessageWrapped } from 'src/app/models/translate/message-wrapped';
-import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -11,30 +10,7 @@ import { ChatService } from 'src/app/services/chat.service';
 export class ChatComponent {
   @Input() messagesWrapped: MessageWrapped[];
   @Output() editMessageEmit = new EventEmitter();
-  public fold: boolean;
-  public messageSelected: number;
-  public foldIcon: string = 'Voir plus';
-  constructor(private chatService: ChatService) {
-    this.fold = false;
-  }
-
-  // public visible: boolean = false;
-
-  // public extand() {
-  //   this.fold = !this.fold;
-  // }
-
-  public deleteMessage(index: number) {
-    this.messagesWrapped.splice(index, 1);
-    this.chatService.messagesStored.splice(index, 1);
-  }
-
-  public editMessage(index: number) {
-    const sentMessage: Message = this.messagesWrapped[index].message;
-    this.editMessageEmit.emit(sentMessage);
-    this.messagesWrapped.splice(index, 1);
-    this.chatService.messagesStored.splice(index, 1);
-  }
+  public messageNumberToFold: number;
 
   public listen(index: number) {
     const sentMessage: Message = this.messagesWrapped[index].message;
@@ -43,8 +19,6 @@ export class ChatComponent {
     }
   }
   public unFold(messageIndex: number) {
-    messageIndex === this.messageSelected ? (this.messageSelected = -1) : (this.messageSelected = messageIndex);
-    this.foldIcon = this.fold ? 'Voir plus' : 'Voir moins';
-    this.fold = !this.fold;
+    messageIndex === this.messageNumberToFold ? (this.messageNumberToFold = -1) : (this.messageNumberToFold = messageIndex);
   }
 }
