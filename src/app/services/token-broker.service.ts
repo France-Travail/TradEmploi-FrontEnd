0589318d66529177e80f36f2dfb4f8bdbcd1a5b7
innostarterkit <innostarterkit@gmail.com>
2021-03-22 10:04:39 +0100

    getFbToken millisecond

--- src/app/services/token-broker.service.ts
+++ src/app/services/token-broker.service.ts
@@ -14,37 +14,35 @@ export class TokenBrokerService {
 
   public getFbToken(email: string, password?: string){
     const jwtFbSingleton = JwtFbSingleton.getInstance()
-    if(jwtFbSingleton.getToken() === null || jwtFbSingleton.getToken().expireTime.isAfter(moment())){
-      const url = `${environment.gcp.identityUrlToken}?key=${environment.firebaseConfig.apiKey}`
-      const data = {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      };
-
-      axios({
-        method: 'POST',
-        headers: { 'content-type': 'application/json; charset=utf-8' },
-        url,
-        data,
-      })
-        .then((response) => {
-          const expiryDate: Moment =  moment().add(response.data.expiresIn, 'seconds')
-          const token:Token = {token: response.data.idToken, expireTime :  expiryDate}
-          jwtFbSingleton.setToken(token)
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
+    if(jwtFbSingleton.getToken() !== null && jwtFbSingleton.getToken().expireTime.isAfter(moment())){
+      return
     }
+    const url = `${environment.gcp.identityUrlToken}?key=${environment.firebaseConfig.apiKey}`
+    const data = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+
+    axios({
+      method: 'POST',
+      headers: { 'content-type': 'application/json; charset=utf-8' },
+      url,
+      data,
+    })
+      .then((response) => {
+        const expiryDate: Moment =  moment().add(response.data.expiresIn, 'milliseconds')
+        const token:Token = {token: response.data.idToken, expireTime :  expiryDate}
+        jwtFbSingleton.setToken(token)
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
   }
 
   public getTokenAdmin(firebaseToken: string) {
     const jwtGwSingleton = JwtGwSingleton.getInstance()
     const jwtGcpSingleton = JwtGcpSingleton.getInstance()
-
-
-    
     if(jwtGwSingleton.getToken()=== null || jwtGwSingleton.getToken().expireTime.isAfter(moment())){
       const url = `${environment.gcp.gateWayUrl}/token`;
       return axios({
