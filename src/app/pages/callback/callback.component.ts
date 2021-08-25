9f3798ac88c545f188641ba1065d521fca2396fc
anishezam <hamza-anis.hezam@pole-emploi.fr>
2021-08-25 17:00:30 +0200

    style login button OK

--- src/app/pages/callback/callback.component.ts
+++ src/app/pages/callback/callback.component.ts
@@ -3,16 +3,15 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChatService } from 'src/app/services/chat.service';
 import { SettingsService } from 'src/app/services/settings.service';
-import { ToastService } from 'src/app/services/toast.service';
 import jwtDecode from 'jwt-decode';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-callback',
   templateUrl: './callback.component.html',
   styleUrls: ['./callback.component.scss'],
 })
 export class CallbackComponent implements OnInit {
-  constructor(private authService: AuthService, private settingsService: SettingsService, private router: Router, private toastService: ToastService, private chatService: ChatService) {}
+  constructor(private authService: AuthService, private settingsService: SettingsService, private router: Router, private chatService: ChatService) {}
 
   ngOnInit(): void {
     const token = this.getToken(window.location.href);
@@ -32,7 +31,7 @@ export class CallbackComponent implements OnInit {
 
   private async loginAuthentificated(emailPe: string) {
     try {
-      const auth = await this.authService.login('anistest@pe.fr', 'rached', emailPe);
+      const auth = await this.authService.login(environment.peama.login, environment.peama.password, emailPe);
       const roomId = this.chatService.getRoomId();
       localStorage.setItem('isLogged', 'true');
       this.authService.role.subscribe((role) => {
