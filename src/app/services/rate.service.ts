1212508f5f205a70948fdffb8dd22d2f4c4be2ba
oghali <oussema.ghali@soprasteria.com>
2021-09-06 14:34:47 +0200

    add new prod env

--- src/app/services/rate.service.ts
+++ src/app/services/rate.service.ts
@@ -40,6 +40,7 @@ export class RateService {
                 efficientGrade
                 offerLinked
                 comment
+                conversationDuration
               }
             }`,
     };
