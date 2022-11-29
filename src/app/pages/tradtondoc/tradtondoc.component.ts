6c1f7e2cad5c1b984d63fb2abebabb434fe579ce
anis <anishezam@gmail.com>
2022-11-29 15:48:00 +0100

    Handling TTS not supported; Error toast; improve UI

--- src/app/pages/tradtondoc/tradtondoc.component.ts
+++ src/app/pages/tradtondoc/tradtondoc.component.ts
@@ -1,12 +1,14 @@
-import {Component, HostListener} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {MatDialog} from '@angular/material';
-import {TextToSpeechService} from '../../services/text-to-speech.service';
-import {TradTonDocService} from '../../services/trad-ton-doc.service';
-import {TranslateService} from '../../services/translate.service';
-import {LoaderComponent} from '../settings/loader/loader.component';
-import {RateDialogComponent} from '../translation/dialogs/rate-dialog/rate-dialog.component';
-import {SettingsService} from './../../services/settings.service';
+import { Component, HostListener } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { VOCABULARY } from 'src/app/data/vocabulary';
+import { ToastService } from 'src/app/services/toast.service';
+import { TextToSpeechService } from '../../services/text-to-speech.service';
+import { TradTonDocService } from '../../services/trad-ton-doc.service';
+import { TranslateService } from '../../services/translate.service';
+import { LoaderComponent } from '../settings/loader/loader.component';
+import { RateDialogComponent } from '../translation/dialogs/rate-dialog/rate-dialog.component';
+import { SettingsService } from './../../services/settings.service';
 
 @Component({
   selector: 'app-tradtondoc',
@@ -26,22 +28,29 @@ export class TradtondocComponent {
   text: string;
   translatedText: string;
   isPlaying: boolean = false;
-
+  showAudioControls: boolean = false;
+  private isAudioSupported: boolean;
   constructor(
     private readonly dialog: MatDialog,
     private readonly translationService: TranslateService,
     private readonly textToSpeechService: TextToSpeechService,
     private readonly tradTonDocService: TradTonDocService,
-    private readonly settingsService: SettingsService
+    private readonly settingsService: SettingsService,
+    private readonly toastService: ToastService
   ) {
     this.settingsService.user.subscribe((user) => {
       if (user && user.language) {
         this.targetLanguage = user.language.written;
+        console.log(user);
       }
     });
+    const language = VOCABULARY.find((i) => i.isoCode === this.targetLanguage || i.audioCode === this.targetLanguage);
+    this.isAudioSupported = language.sentences.audioSupported !== undefined;
   }
 
   onChange(event: any) {
+    this.audioFile = null;
+    this.showAudioControls = false;
     this.translatedText = null;
     this.file = event.target.files[0];
     this.fileName = this.file.name;
@@ -55,19 +64,31 @@ export class TradtondocComponent {
 
   async onSubmit() {
     if (this.file && this.fileName) {
-      const loaderDialog = this.dialog.open(LoaderComponent, {panelClass: 'loader', disableClose: true});
-      const result = await this.tradTonDocService.detectText(this.fileName, this.file).finally(() => loaderDialog.close());
+      const loaderDialog = this.dialog.open(LoaderComponent, { panelClass: 'loader', disableClose: true });
+      const result = await this.tradTonDocService
+        .detectText(this.fileName, this.file)
+        .catch((err) => {
+          loaderDialog.close();
+          this.toastService.showToast('Une erreur est survenue, veuillez réessayer plus tard', 'toast-error');
+          console.log(err);
+        })
+        .finally(() => loaderDialog.close());
       this.text = result.text;
       if (this.text.length > 0) {
         this.translatedText = await this.translationService.translate(this.text, this.targetLanguage);
-        await this.textToSpeechService
-          .getSpeech(this.translatedText, this.targetLanguage)
-          .then((_) => {
-            this.audioFile = this.textToSpeechService.audioSpeech;
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+        if (this.isAudioSupported) {
+          await this.textToSpeechService
+            .getSpeech(this.translatedText, this.targetLanguage)
+            .then((_) => {
+              this.showAudioControls = true;
+              this.audioFile = this.textToSpeechService.audioSpeech;
+            })
+            .catch((err) => {
+              this.audioFile = null;
+              this.toastService.showToast("L'audio n'a pas pu être generé.", 'toast-error');
+              console.log(err);
+            });
+        }
         loaderDialog.close();
       }
     }
