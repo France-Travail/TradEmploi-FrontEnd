85704ac472d6c65a0a522f9f4febf7cfc6a6b6e7
EOGH2300 <oussema.ghali-ext@pole-emploi.fr>
2022-12-09 14:57:29 +0100

    TRAD-558 fix regression - add rating stars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,80 +1,83 @@
 // Angular
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { LayoutModule } from '@angular/cdk/layout';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {LayoutModule} from '@angular/cdk/layout';
 // Keyboard
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
-import { MatCardModule } from '@angular/material/card';
+import {MatCardModule} from '@angular/material/card';
 // Handle Navigation Tree
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 // Handle firebase connection
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireFunctionsModule} from '@angular/fire/functions';
 // Environment loaded
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
 // Handle Animations for Angular Material
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // Allow to use French date format
-import { MAT_DATE_LOCALE, MatGridListModule, MatSortModule, MatTableModule } from '@angular/material';
+import {MAT_DATE_LOCALE, MatGridListModule, MatSortModule, MatTableModule} from '@angular/material';
 // Import shared module and components
-import { SharedModule } from './shared/shared.module';
-import { LogoutComponent } from './shared/components/logout/logout.component';
-import { ShareComponent } from './pages/translation/dialogs/share/share.component';
-import { AuthorizeComponent } from './pages/translation/dialogs/authorize/authorize.component';
-import { WelcomeDeComponent } from './pages/translation/dialogs/welcome-de/welcome-de.component';
-import { EndComponent } from './pages/translation/dialogs/end/end.component';
+import {SharedModule} from './shared/shared.module';
+import {LogoutComponent} from './shared/components/logout/logout.component';
+import {ShareComponent} from './pages/translation/dialogs/share/share.component';
+import {AuthorizeComponent} from './pages/translation/dialogs/authorize/authorize.component';
+import {WelcomeDeComponent} from './pages/translation/dialogs/welcome-de/welcome-de.component';
+import {EndComponent} from './pages/translation/dialogs/end/end.component';
 // Main Components
-import { AppComponent } from './app.component';
-import { StartComponent } from './pages/start/start.component';
-import { TranslationComponent } from './pages/translation/translation.component';
-import { HistoricComponent } from './pages/historic/historic.component';
+import {AppComponent} from './app.component';
+import {StartComponent} from './pages/start/start.component';
+import {TranslationComponent} from './pages/translation/translation.component';
+import {HistoricComponent} from './pages/historic/historic.component';
 // Dialogs
-import { LanguageGridComponent } from './pages/choice/language/grid/language-grid.component';
-import { MeetingComponent } from './pages/translation/dialogs/meeting/meeting.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { RemoveComponent } from './pages/historic/dialogs/remove/remove.component';
-import { ShowComponent } from './pages/historic/dialogs/show/show.component';
-import { ThanksComponent } from './pages/thanks/thanks.component';
-import { ConversationComponent } from './pages/conversation/conversation.component';
-import { MessageWrapperComponent } from './pages/translation/components/message-wrapper/message-wrapper.component';
-import { RecordComponent } from './pages/translation/components/record/record.component';
-import { ChatComponent } from './pages/translation/components/chat/chat.component';
-import { RateDialogComponent } from './pages/translation/dialogs/rate-dialog/rate-dialog.component';
-import { AuthenticationComponent } from './pages/authentication/authentication.component';
-import { AnonymousComponent } from './pages/anonymous/anonymous.component';
-import { GdprComponent } from './pages/gdpr/gdpr.component';
-import { DeviceDetectorModule } from 'ngx-device-detector';
-import { NavbarService } from './services/navbar.service';
-import { ChatMultiDevicesComponent } from './pages/translation/components/chat-multi-devices/chat-multi-devices.component';
-import { QRCodeModule } from 'angularx-qrcode';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { LoaderComponent } from './pages/settings/loader/loader.component';
-import { ChoiceComponent } from './pages/choice/choice.component';
-import { ModalityComponent } from './pages/modality/modality.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { OnboardingComponent } from './pages/translation/dialogs/onboarding/onboarding.component';
-import { OAuthModule } from 'angular-oauth2-oidc';
-import { CallbackComponent } from './pages/callback/callback.component';
-import { CommonModule } from '@angular/common';
-import { CarouselComponent } from './pages/translation/dialogs/onboarding/carousel/carousel.component';
-import { IndicatorsComponent } from './indicators/indicators.component';
-import { BarChartModule, PieChartModule } from '@swimlane/ngx-charts';
-import { VoicesService } from './services/voices.service';
-import { ErrorService } from './services/error.service';
-import { TokenBrokerService } from './services/token-broker.service';
-import { TextToSpeechMicrosoftService } from './services/text-to-speech-microsoft.service';
-import { TextToSpeechGcpService } from './services/text-to-speech-gcp.service';
-import { TextToSpeechService } from './services/text-to-speech.service';
-import { TokenAzureService } from './services/token-azure.service';
-import { TradtondocComponent } from './pages/tradtondoc/tradtondoc.component';
-import { ImageCropperModule } from 'ngx-image-cropper';
-import { DragAndDropDirective } from './pages/tradtondoc/draganddrop.directive';
+import {LanguageGridComponent} from './pages/choice/language/grid/language-grid.component';
+import {MeetingComponent} from './pages/translation/dialogs/meeting/meeting.component';
+import {SettingsComponent} from './pages/settings/settings.component';
+import {RemoveComponent} from './pages/historic/dialogs/remove/remove.component';
+import {ShowComponent} from './pages/historic/dialogs/show/show.component';
+import {ThanksComponent} from './pages/thanks/thanks.component';
+import {ConversationComponent} from './pages/conversation/conversation.component';
+import {MessageWrapperComponent} from './pages/translation/components/message-wrapper/message-wrapper.component';
+import {RecordComponent} from './pages/translation/components/record/record.component';
+import {ChatComponent} from './pages/translation/components/chat/chat.component';
+import {RateDialogComponent} from './pages/translation/dialogs/rate-dialog/rate-dialog.component';
+import {AuthenticationComponent} from './pages/authentication/authentication.component';
+import {AnonymousComponent} from './pages/anonymous/anonymous.component';
+import {GdprComponent} from './pages/gdpr/gdpr.component';
+import {DeviceDetectorModule} from 'ngx-device-detector';
+import {NavbarService} from './services/navbar.service';
+import {
+  ChatMultiDevicesComponent
+} from './pages/translation/components/chat-multi-devices/chat-multi-devices.component';
+import {QRCodeModule} from 'angularx-qrcode';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {LoaderComponent} from './pages/settings/loader/loader.component';
+import {ChoiceComponent} from './pages/choice/choice.component';
+import {ModalityComponent} from './pages/modality/modality.component';
+import {WelcomeComponent} from './pages/welcome/welcome.component';
+import {OnboardingComponent} from './pages/translation/dialogs/onboarding/onboarding.component';
+import {OAuthModule} from 'angular-oauth2-oidc';
+import {CallbackComponent} from './pages/callback/callback.component';
+import {CommonModule} from '@angular/common';
+import {CarouselComponent} from './pages/translation/dialogs/onboarding/carousel/carousel.component';
+import {IndicatorsComponent} from './indicators/indicators.component';
+import {BarChartModule, PieChartModule} from '@swimlane/ngx-charts';
+import {VoicesService} from './services/voices.service';
+import {ErrorService} from './services/error.service';
+import {TokenBrokerService} from './services/token-broker.service';
+import {TextToSpeechMicrosoftService} from './services/text-to-speech-microsoft.service';
+import {TextToSpeechGcpService} from './services/text-to-speech-gcp.service';
+import {TextToSpeechService} from './services/text-to-speech.service';
+import {TokenAzureService} from './services/token-azure.service';
+import {TradtondocComponent} from './pages/tradtondoc/tradtondoc.component';
+import {ImageCropperModule} from 'ngx-image-cropper';
+import {DragAndDropDirective} from './pages/tradtondoc/draganddrop.directive';
+import {NgRatingBarModule} from 'ng-rating-bar';
 
 @NgModule({
   declarations: [
@@ -140,6 +143,7 @@ import { DragAndDropDirective } from './pages/tradtondoc/draganddrop.directive';
     BarChartModule,
     PieChartModule,
     ImageCropperModule,
+    NgRatingBarModule,
   ],
   providers: [
     NavbarService,
@@ -156,7 +160,8 @@ import { DragAndDropDirective } from './pages/tradtondoc/draganddrop.directive';
   bootstrap: [AppComponent],
   entryComponents: [MeetingComponent, RemoveComponent, ShowComponent, RateDialogComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
 
 export function TextToSpeechFactory(voicesService: VoicesService, errorService: ErrorService, tbs: TokenBrokerService, tokenAzureService: TokenAzureService) {
   if (environment.microsoftSpeechConfig.textToSpeechEnabled) {
