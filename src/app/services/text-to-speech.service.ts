969b33a4b59d99eeb9f6f161e380c5d3c48d8beb
anis <anishezam@gmail.com>
2022-12-28 09:20:26 +0100

    WIP

--- src/app/services/text-to-speech.service.ts
+++ src/app/services/text-to-speech.service.ts
@@ -2,36 +2,39 @@ export abstract class TextToSpeechService {
   public audioSpeech: HTMLAudioElement = undefined;
   public isPlaying: boolean = false;
   private sentence: string = undefined;
+  private isFemaleSpeech: boolean = false;
   abstract getSpeech(text: string, language: string, isFemaleSpeech: boolean): Promise<void>;
 
   public play = async (sentence: string, audioCode: string, playAuto: boolean = true, isFemaleSpeech: boolean = false) => {
     if (!this.isPlaying) {
-      if (this.sentence !== sentence) {
-        this.sentence = sentence;
-        await this.getSpeech(sentence, audioCode, isFemaleSpeech).then(
-          () => {
-            if (playAuto) {
-              this.audioSpeech.play();
-              this.audioSpeech.onplay = () => {
-                this.isPlaying = true;
-              };
-              this.audioSpeech.onended = () => {
-                this.isPlaying = false;
-              };
+      // if (this.sentence !== sentence || this.isFemaleSpeech !== isFemaleSpeech) {
+        if (this.sentence !== sentence ) {
+          this.sentence = sentence;
+          this.isFemaleSpeech = isFemaleSpeech;
+          await this.getSpeech(sentence, audioCode, isFemaleSpeech).then(
+            () => {
+              if (playAuto) {
+                this.audioSpeech.play();
+                this.audioSpeech.onplay = () => {
+                  this.isPlaying = true;
+                };
+                this.audioSpeech.onended = () => {
+                  this.isPlaying = false;
+                };
+              }
+            },
+            (error) => {
+              console.log(error);
             }
-          },
-          (error) => {
-            console.log(error);
+          );
+        } else {
+          if (playAuto) {
+            this.audioSpeech.play();
           }
-        );
-      } else {
-        if (playAuto) {
-          this.audioSpeech.play();
         }
-      }
     }
     return this.audioSpeech;
-  }
+  };
 
   public stop = () => {
     if (this.audioSpeech) {
@@ -39,5 +42,5 @@ export abstract class TextToSpeechService {
       this.audioSpeech.currentTime = 0;
       this.isPlaying = false;
     }
-  }
+  };
 }
