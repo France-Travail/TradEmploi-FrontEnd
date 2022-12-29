2526f29ca2dc9e430d85979ae7e011861360bbbc
Sébastien LAFAYE <sebastien.lafaye@pole-emploi.fr>
2022-12-29 16:03:08 +0100

    TRAD-563 add pdf to image service

--- src/app/pages/tradtondoc/tradtondoc.component.ts
+++ src/app/pages/tradtondoc/tradtondoc.component.ts
@@ -1,16 +1,17 @@
-import { NavbarService } from 'src/app/services/navbar.service';
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material';
-import { VOCABULARY } from 'src/app/data/vocabulary';
-import { ToastService } from 'src/app/services/toast.service';
-import { TextToSpeechService } from '../../services/text-to-speech.service';
-import { TradTonDocService } from '../../services/trad-ton-doc.service';
-import { TranslateService } from '../../services/translate.service';
-import { LoaderComponent } from '../settings/loader/loader.component';
-import { RateDialogComponent } from '../translation/dialogs/rate-dialog/rate-dialog.component';
-import { SettingsService } from './../../services/settings.service';
-import { ImageCroppedEvent } from 'ngx-image-cropper';
+import {NavbarService} from 'src/app/services/navbar.service';
+import {Component, HostListener, OnDestroy} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material';
+import {VOCABULARY} from 'src/app/data/vocabulary';
+import {ToastService} from 'src/app/services/toast.service';
+import {TextToSpeechService} from '../../services/text-to-speech.service';
+import {TradTonDocService} from '../../services/trad-ton-doc.service';
+import {TranslateService} from '../../services/translate.service';
+import {LoaderComponent} from '../settings/loader/loader.component';
+import {RateDialogComponent} from '../translation/dialogs/rate-dialog/rate-dialog.component';
+import {SettingsService} from '../../services/settings.service';
+import {PdfToImageService} from '../../services/pdf-to-image.service';
+import {ImageCroppedEvent} from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-tradtondoc',
@@ -27,7 +28,7 @@ export class TradtondocComponent implements OnDestroy {
   fileName: string;
   file: File;
   text: string;
-  isConform: boolean;
+  isConform: boolean = false;
   translatedText: string;
   audioFile: HTMLAudioElement;
   isPlaying: boolean = false;
@@ -37,12 +38,14 @@ export class TradtondocComponent implements OnDestroy {
   private isAudioSupported: boolean;
   targetCountry: string;
   nbTranslatedCharacters: number = 0;
+  isConvertedFile: boolean = false;
 
   constructor(
     private readonly dialog: MatDialog,
     private readonly translationService: TranslateService,
     private readonly textToSpeechService: TextToSpeechService,
     private readonly tradTonDocService: TradTonDocService,
+    private readonly convertPdfToImageService: PdfToImageService,
     private readonly settingsService: SettingsService,
     private readonly toastService: ToastService,
     private readonly navService: NavbarService
@@ -66,34 +69,15 @@ export class TradtondocComponent implements OnDestroy {
 
   async onSubmit() {
     if (this.file && this.fileName) {
-      const loaderDialog = this.dialog.open(LoaderComponent, { panelClass: 'loader', disableClose: true });
-      const result = await this.tradTonDocService
-        .detectText(this.fileName, this.croppedImage ? this.croppedImage : this.file)
-        .catch((err) => {
-          loaderDialog.close();
-          this.toastService.showToast('Une erreur est survenue, veuillez réessayer plus tard', 'toast-error');
-          console.log(err);
-        })
-        .finally(() => loaderDialog.close());
+      const detectText = await this.detectText(
+          this.isConvertedFile ? `${this.fileName.split('.')[0]}.png` : this.fileName, this.croppedImage ? this.croppedImage : this.file);
       this.croppedImage = null;
-      this.text = result ? result.text : '';
-      this.nbTranslatedCharacters += result ? result.numberCharactersInText : 0;
+      this.text = detectText ? detectText.text : '';
+      this.nbTranslatedCharacters += detectText ? detectText.numberCharactersInText : 0;
       this.isConform = this.checkNumberTranslatedCharacters(this.nbTranslatedCharacters);
       if (this.text.length > 0) {
         this.translatedText = await this.translationService.translate(this.text, this.targetLanguage);
-        if (this.isAudioSupported) {
-          await this.textToSpeechService
-            .play(this.translatedText, this.targetLanguage, false)
-            .then((_) => {
-              this.audioFile = this.textToSpeechService.audioSpeech;
-              this.showAudioControls = true;
-            })
-            .catch((err) => {
-              this.toastService.showToast("L'audio n'a pas pu être generé.", 'toast-error');
-              console.log(err);
-            });
-        }
-        loaderDialog.close();
+        await this.textToSpeech();
       }
     }
   }
@@ -134,27 +118,29 @@ export class TradtondocComponent implements OnDestroy {
     this.croppedImage = $event.base64;
     this.translatedText = null;
   }
+
   async onFileDropped($event) {
     const file = $event;
-    this.isConform = false;
-    this.isPdf = false;
     this.prepareFile(file);
-    this.applyControls(file);
+    await this.applyControls(file);
     this.imageChangedEvent = { target: { files: [file] } };
   }
 
-  fileBrowseHandler($event) {
+  async fileBrowseHandler($event) {
     const files = $event.target.files[0];
     this.prepareFile(files);
-    this.applyControls(files);
+    await this.applyControls(files);
     this.imageChangedEvent = $event;
   }
 
-  applyControls(file) {
+  async applyControls(file) {
     if (file.type === 'application/pdf') {
       this.isPdf = true;
       this.isConform = this.checkFileSize(file.size) && this.checkTypeFile(file.type);
-      this.assertOnePage();
+      this.assertOnePage(file);
+      this.file = await this.convertPdfToImage();
+      this.isPdf = !!this.file;
+      this.isConvertedFile = !!this.file;
     } else {
       this.isPdf = false;
       this.isConform = this.checkFileSize(file.size) && this.checkTypeFile(file.type);
@@ -164,7 +150,7 @@ export class TradtondocComponent implements OnDestroy {
   checkTypeFile(type) {
     const allowedTypes = ['application/pdf', 'image/png', 'image/jpeg', 'image/jpg'];
     if (!allowedTypes.includes(type)) {
-      this.toastService.showToast("Fichier non conforme. Ce type de fichier n'est pas pris en charge", 'toast-warning');
+      this.toastService.showToast('Fichier non conforme. Ce type de fichier n\'est pas pris en charge', 'toast-warning');
       return false;
     }
     return true;
@@ -179,13 +165,13 @@ export class TradtondocComponent implements OnDestroy {
     return true;
   }
 
-  assertOnePage() {
+  assertOnePage(file) {
     const reader = new FileReader();
-    reader.readAsBinaryString(this.file);
+    reader.readAsBinaryString(file);
     reader.onloadend = () => {
       const numberPagesDoc = (reader.result as string).match(/\/Type[\s]*\/Page[^s]/g).length;
       if (numberPagesDoc > 1) {
-        this.toastService.showToast("Fichier non conforme. Le PDF fourni contient plus d'une page ", 'toast-warning');
+        this.toastService.showToast('Fichier non conforme. Le PDF fourni contient plus d\'une page ', 'toast-warning');
         this.isConform = false;
       } else {
         this.isConform = true;
@@ -207,9 +193,50 @@ export class TradtondocComponent implements OnDestroy {
     this.translatedText = null;
     this.fileName = this.file.name;
     const reader = new FileReader();
-    reader.onload = (e: any) => {
+    reader.onload = async (e: any) => {
       this.file = e.target.result;
     };
     reader.readAsDataURL(this.file);
+  }
+
+  async convertPdfToImage() {
+    const loaderDialog = this.dialog.open(LoaderComponent, { panelClass: 'loader', disableClose: true });
+    return await this.convertPdfToImageService
+        .convertPdfToImage(this.fileName, this.file)
+        .catch((error) => {
+          loaderDialog.close();
+          this.toastService.showToast('Une erreur est survenue, veuillez réessayer plus tard', 'toast-error');
+          console.log(error);
+        })
+        .finally(() => loaderDialog.close());
+  }
+
+  async detectText(fileName, data) {
+    const loaderDialog = this.dialog.open(LoaderComponent, { panelClass: 'loader', disableClose: true });
+    return await this.tradTonDocService
+      .detectText(fileName, data)
+      .catch((err) => {
+        loaderDialog.close();
+        this.toastService.showToast('Une erreur est survenue, veuillez réessayer plus tard', 'toast-error');
+        console.log(err);
+      })
+      .finally(() => loaderDialog.close());
+  }
+
+  async textToSpeech() {
+    const loaderDialog = this.dialog.open(LoaderComponent, { panelClass: 'loader', disableClose: true });
+    if (this.isAudioSupported) {
+      await this.textToSpeechService
+        .play(this.translatedText, this.targetLanguage, false)
+        .then((_) => {
+          this.audioFile = this.textToSpeechService.audioSpeech;
+          this.showAudioControls = true;
+        })
+        .catch((err) => {
+          this.toastService.showToast('L\'audio n\'a pas pu être generé.', 'toast-error');
+          console.log(err);
+        });
+      }
+    loaderDialog.close();
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import {Role} from './../models/role';
+import {Role} from '../models/role';
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {FbAuthSingleton} from '../models/token/FbAuthSingleton';
