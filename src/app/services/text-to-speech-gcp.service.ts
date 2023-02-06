b8bc43de9b7c8c86d98e9db685d085f471c111f0
EOGH2300 <oussema.ghali-ext@pole-emploi.fr>
2023-02-06 15:58:36 +0100

    specify a regional endpoint = europe for the gcp text to speech synthesis

--- src/app/services/text-to-speech-gcp.service.ts
+++ src/app/services/text-to-speech-gcp.service.ts
@@ -40,7 +40,7 @@ export class TextToSpeechGcpService extends TextToSpeechService {
   getSpeech = async (text: string, language: string, isFemaleSpeech: boolean): Promise<void> => {
     this.audioSpeech = undefined;
     const tokenResponse: TokenResponse = await this.tbs.getTokenGcp();
-    const urlRecognize = 'https://texttospeech.googleapis.com/v1/text:synthesize';
+    const urlRecognize = 'https://eu-texttospeech.googleapis.com/v1/text:synthesize';
     let voiceSelected: Voice[] = VOICES.filter((v) => v.languageCodes.includes(language));
     if (voiceSelected.length === 0) {
       const apiVoiceResult = await this.voicesService.getVoices();
