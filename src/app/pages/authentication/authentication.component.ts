c816429094a166b26702f10be8a68c30d07c196a
maxime_alza <m.alza@alltechconsulting.fr>
2024-06-10 16:19:05 +0200

    Update authentication component

--- src/app/pages/authentication/authentication.component.ts
+++ src/app/pages/authentication/authentication.component.ts
@@ -1,44 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {OAuthService} from 'angular-oauth2-oidc';
-import {JwksValidationHandler} from 'angular-oauth2-oidc-jwks';
-import {authCodeFlowConfig} from '../../../environments/authflow';
+import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
+import {AbstractControl, UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import {params} from '../../../environments/params';
-import {ERROR_FUNC_LOGIN_OR_PASSWORD} from '../../models/error/errorFunctionnal';
+import {ERROR_FUNC_LOGIN_OR_PASSWORD, ERROR_FUNC_OAUTH} from '../../models/error/errorFunctionnal';
 import {AuthService} from '../../services/auth.service';
 import {ChatService} from '../../services/chat.service';
 import {ToastService} from '../../services/toast.service';
 import {SettingsService} from '../../services/settings.service';
+import {FbAuthSingleton} from '../../models/token/FbAuthSingleton';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { AuthType } from '../../models/AuthType';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['../../../sass/panel.scss'],
+  styleUrls: ['./authentication.component.scss', '../../../sass/panel.scss'],
 })
 export class AuthenticationComponent implements OnInit {
-  public form: FormGroup;
-  public isOauthLogin: boolean = authCodeFlowConfig.loginUrl !== undefined;
-  public oAuthProvider: string = params.organization.name;
 
   constructor(
-    private readonly oauthService: OAuthService,
     private readonly router: Router,
-    private readonly fb: FormBuilder,
+    private readonly route: ActivatedRoute,
+    private readonly fb: UntypedFormBuilder,
     private readonly authService: AuthService,
     private readonly chatService: ChatService,
     private readonly toastService: ToastService,
-    private readonly settingsService: SettingsService
-  ) {
-    this.configureSSO();
-  }
-
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.minLength(6), Validators.required]],
-    });
-  }
+    private readonly settingsService: SettingsService,
+    private readonly cd: ChangeDetectorRef,
+    private readonly zone: NgZone
+  ) { }
 
   get email(): AbstractControl {
     return this.form.get('email');
@@ -48,8 +39,27 @@ export class AuthenticationComponent implements OnInit {
     return this.form.get('password');
   }
 
+  public form: UntypedFormGroup;
+  public isOauthLogin: boolean = Boolean(params.oauthIdProvider);
+  public showOauthLoginError: boolean ;
+  public oauthIdProvider: string = params.oauthIdProvider;
+  public authPending = false;
+  private disconnected = false;
+
+  protected readonly params = params;
+
+  ngOnInit(): void {
+    this.route.queryParams.forEach((params: Params) => {
+      this.disconnected = params['disconnected'];
+    });
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6), Validators.required]],
+    });
+  }
+
   public async onSubmit(): Promise<void> {
-    if (!this.isOauthLogin) {
+    if (!this.isOauthLogin || params.authType === AuthType.both || params.authType === AuthType.loginPassword) {
       try {
         const auth = await this.authService.login(this.email.value, this.password.value, true);
         if (auth.isAuth) {
@@ -66,7 +76,7 @@ export class AuthenticationComponent implements OnInit {
             isMultiDevices: false,
           });
           localStorage.setItem('user', JSON.stringify(this.settingsService.user.value));
-          this.router.navigateByUrl('choice');
+          this.redirectPostAuth();
         }
       } catch (error) {
         this.toastService.showToast(ERROR_FUNC_LOGIN_OR_PASSWORD.description, 'toast-error');
@@ -74,13 +84,70 @@ export class AuthenticationComponent implements OnInit {
     }
   }
 
-  configureSSO() {
-    this.oauthService.configure(authCodeFlowConfig);
-    this.oauthService.oidc = true;
-    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+  oauthLogin() {
+    this.authPending = true;
+    this.showOauthLoginError = false;
+    firebase.auth().getRedirectResult()
+      .then(value => {
+        if (value?.user !== undefined && value?.user !== null) {
+          try {
+            const roomId = this.chatService.createRoomId();
+            localStorage.setItem('isLogged', 'true');
+            const userEmail = value.additionalUserInfo.profile["Mail"];
+            this.settingsService.user.next({
+              ...this.settingsService.user.value,
+              role: this.authService.getRole(userEmail),
+              firstname: params.organization.organizationUser,
+              lastname: params.organization.name,
+              email: userEmail,
+              connectionTime: Date.now(),
+              roomId,
+              isMultiDevices: false,
+            });
+            FbAuthSingleton.getInstance().setFbAuth(value);
+            localStorage.setItem('user', JSON.stringify(this.settingsService.user.value));
+            this.redirectPostAuth();
+          } catch (error) {
+            this.oauthLoginHandleError();
+          }
+        } else {
+          this.authPending = false;
+          if (params.authType == AuthType.SSO && !this.disconnected) {
+            const provider = new firebase.auth.OAuthProvider(this.oauthIdProvider);
+            firebase.auth().signInWithRedirect(provider);
+          }
+        }
+      }, reason => this.oauthLoginHandleError());
   }
 
-  login() {
-    this.oauthService.initCodeFlow();
+  oauthLoginHandleError(): void {
+    this.authPending = false;
+    this.showOauthLoginError = true;
+    this.cd.detectChanges();
+    this.toastService.showToast(ERROR_FUNC_OAUTH.description, 'toast-error');
   }
+
+  private redirectPostAuth() {
+    this.zone.run(() => {
+      // On récupère l'url demandée avant le process d'authentification
+      const redirectUrl: string = sessionStorage.getItem('redirectUrl');
+      sessionStorage.removeItem('redirectUrl');
+
+      // Si l'url est alimentée que ce n'est pas les pages de chargement alors on redirige
+      // sinon on va à la page de choix de la langue
+      if (redirectUrl !== undefined && redirectUrl !== null && !redirectUrl.includes('start') && !redirectUrl.includes('auth')) {
+        this.router.navigateByUrl(redirectUrl);
+      } else {
+        this.router.navigateByUrl('choice');
+      }
+    });
+  }
+
+  public connexionTraduction() {
+    this.authPending = true;
+    const provider = new firebase.auth.OAuthProvider(this.oauthIdProvider);
+    firebase.auth().signInWithRedirect(provider);
+  }
+
+  protected readonly AuthType = AuthType;
 }
