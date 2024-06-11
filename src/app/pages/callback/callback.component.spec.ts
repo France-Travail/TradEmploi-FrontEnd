25a64a3b943fdc99baff1214d8e19a7c339fff57
maxime_alza <m.alza@alltechconsulting.fr>
2024-06-11 09:20:14 +0200

    Update callback spec component

--- src/app/pages/callback/callback.component.spec.ts
+++ src/app/pages/callback/callback.component.spec.ts
@@ -21,7 +21,7 @@ describe('CallbackComponent', () => {
     id: '123',
     roomId: '1345',
     role: Role.GUEST,
-    firstname: 'SNCF',
+    firstname: 'TRADUCTION',
     isMultiDevices: true,
     email: 'test@gmail.com',
     idDGASI: '1',
