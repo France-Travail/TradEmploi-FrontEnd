096381840eb07b847bb6079f240d975b2a2282f0
maxime_alza <m.alza@alltechconsulting.fr>
2024-06-11 09:20:26 +0200

    Update language grid spec component

--- src/app/pages/choice/language/grid/language-grid.component.spec.ts
+++ src/app/pages/choice/language/grid/language-grid.component.spec.ts
@@ -36,7 +36,7 @@ describe('LanguageGridComponent', () => {
     id: '123',
     roomId: '1345',
     role: Role.GUEST,
-    firstname: 'SNCF',
+    firstname: 'TRADUCTION',
     isMultiDevices: true,
     email: 'test@gmail.com',
     idDGASI: '1',
