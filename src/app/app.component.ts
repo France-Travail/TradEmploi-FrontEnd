d618be13d3427d53df0eaaa384b9ffed36e4f799
Maxime Alza <maxime.alza-ext@francetravail.fr>
2025-02-05 16:38:58 +0100

    Update cache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarService } from './services/navbar.service';
-import { VersionCheckService } from './services/version-check.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +12,7 @@ export class AppComponent implements OnInit {
   protected showCreateShortcut = false;
   protected deferredPrompt: any;
 
-  constructor(private readonly router: Router, public readonly navbarService: NavbarService, private versionCheckService: VersionCheckService) {
+  constructor(private readonly router: Router, public readonly navbarService: NavbarService) {
     addEventListener('storage', this.storageChange.bind(this));
   }
 
@@ -27,8 +26,6 @@ export class AppComponent implements OnInit {
     window.addEventListener('beforeinstallprompt', (e) => {
       e.preventDefault();
       this.deferredPrompt = e;
-      this.showCreateShortcut = true;
     });
-    this.versionCheckService.checkVersion('/assets/version.json');
   }
 }
